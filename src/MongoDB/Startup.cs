f91e3fd7f198cb358e4cad521322f4db3c4accdd
Hamed Iravanchi <iravanchi@gmail.com>
2018-02-28 17:04:29 +0330

    Add Detectors.MongoDB empty project

--- src/MongoDB/Startup.cs
+++ src/MongoDB/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Detectors.Redis
+namespace Detectors.MongoDB
 {
     public class Startup
     {
