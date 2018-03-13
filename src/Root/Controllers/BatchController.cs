9480246c66228e8d7c32ba423f0faf2cb7dd952c
Hamed Iravanchi <iravanchi@gmail.com>
2018-03-13 14:47:42 +0330

    Add controllers and dto for batch and calculation actions

---
+++ src/Root/Controllers/BatchController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Root.Controllers.Dto;
+
+namespace Root.Controllers
+{
+    public class BatchController : Controller
+    {
+        [HttpPost("batch")]
+        public IActionResult RunBatch(RunBatchRequest request)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/Root/Controllers/Dto/CalculateSumRequestItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Root.Controllers.Dto
+{
+    public class CalculateSumRequestItem
+    {
+        public string Uri { get; set; }
+        public double Coefficient { get; set; }
+    }
+}
