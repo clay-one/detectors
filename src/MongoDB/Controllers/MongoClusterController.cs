e0afc9a209f4ead1c25e0371501cef94a5df492b
Hossein Taleghani <hossein.t@fanap.plus>
2018-07-31 19:54:19 +0430

    Add GetReplicaSetStatus action

--- src/MongoDB/Controllers/MongoClusterController.cs
+++ src/MongoDB/Controllers/MongoClusterController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using Detectors.MongoDB.Configuration;
+using Detectors.MongoDB.Models;
+using Detectors.MongoDB.Controllers.Dto;
 using Detectors.MongoDB.Util;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
@@ -11,6 +13,7 @@ namespace Detectors.MongoDB.Controllers
     public class MongoClusterController : Controller
     {
         private readonly MongoClusterConfigCollection _configuration;
+
         public MongoClusterController(MongoClusterConfigCollection configuration)
         {
             _configuration = configuration;
@@ -22,9 +25,32 @@ namespace Detectors.MongoDB.Controllers
         {
             var client = _configuration.GetClient(clusterId);
             var databases = (await client.ListDatabasesAsync()).ToEnumerable().Select(d => d.ToJObject()).ToList();
-            
+
             return Ok(databases);
         }
-    
+        
+        [HttpGet("replicaset/status")]
+        [HttpGet("replicaset/status.{format}")]
+        public async Task<IActionResult> GetReplicaSetStatus(string clusterId)
+        {
+            var client = _configuration.GetDirectClient(clusterId);
+            ReplicaSetStatus replSetStatus;
+
+            try
+            {
+                replSetStatus = await client.GetDatabase("admin").RunCommandAsync(new ObjectCommand<ReplicaSetStatus>(new { replSetGetStatus = true }));
+            }
+            catch (MongoCommandException e)
+            {
+                return BadRequest(new MongoCommandError
+                {
+                    Code = e.Code,
+                    CodeName = e.CodeName,
+                    Message = e.ErrorMessage
+                });
+            }
+
+            return Ok(replSetStatus);            
+        }
     }
 }
